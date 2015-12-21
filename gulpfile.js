5b72ebff4763c18979e5b5e141f0855500dcaa8e
Michael Kühnel <m.kuehnel@micromata.de>
2015-12-21 23:33:49 +0100

    Compile and autoprefix Less with sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('hello', function(done) {
-
-	console.log('Hello Micha');
-	done();
-
+gulp.task('less', function() {
+	return gulp.src('./assets/less/index.less')
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(autoprefixer({
+			browsers: [
+					'> 1%',
+					'last 3 version',
+					'ie 8',
+					'ie 9',
+					'Firefox ESR',
+					'Opera 12.1'
+				]
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./assets/css'));
 });
