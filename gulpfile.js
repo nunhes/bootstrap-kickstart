3446743f2e58aceb54d848df11719c9bde879894
Michael Kühnel <m.kuehnel@micromata.de>
2015-12-30 13:42:02 +0100

    Validate html

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var del = require('del');
 var frontmatter = require('gulp-front-matter');
 var handlebars = require('gulp-hb');
 var rename = require('gulp-rename');
+var validator = require('gulp-html');
+var logger = require('gulp-logger');
 
 // Configurable paths
 var	config = {
@@ -15,10 +17,20 @@ var	config = {
 	server: 'server'
 };
 
+/*
+ * ***********************
+ * ***** Clean tasks *****
+ * ***********************
+ */
 gulp.task('cleanServer', function() {
 	return del(['./' + config.server + '/**/*']);
 });
 
+/*
+ * **********************
+ * ***** Copy tasks *****
+ * **********************
+ */
 gulp.task('copyAssetsToServer', function() {
 	return gulp.src([
 					config.src + 'assets/css/**/*',
@@ -42,7 +54,12 @@ gulp.task('copyLibsToServer', function() {
 		.pipe(gulp.dest('./' + config.server));
 });
 
-gulp.task('styles', function() {
+/*
+ * *********************
+ * ***** CSS tasks *****
+ * *********************
+ */
+gulp.task('css', function() {
 	return gulp.src(config.src + 'assets/less/index.less')
 		.pipe(sourcemaps.init())
 		.pipe(less())
@@ -63,6 +80,11 @@ gulp.task('styles', function() {
 		.pipe(gulp.dest('./assets/css'));
 });
 
+/*
+ * ********************
+ * ***** JS tasks *****
+ * ********************
+ */
 gulp.task('scripts', function(done) {
 	console.log('Nothing happening yet.');
 	done();
@@ -81,16 +103,32 @@ gulp.task('html', function () {
 			partials: config.src + 'partials/*.hbs'
 		}))
 		.pipe(rename({extname: '.html'}))
-		.pipe(gulp.dest('./' + config.server));
+		// .pipe(validator());
+		.pipe(gulp.dest('./' + config.server))
 });
 
-/**
- * A task for development
+gulp.task('htmllint', function () {
+	return gulp.src('./' + config.server + '/*.html')
+		.pipe(logger({
+			before: 'Starting validator',
+			after: 'Validator complete!'
+		}))
+		.pipe(validator())
+		.on('error', notify.onError({
+			message: '\nError: <%= error.message %>'
+		}));
+});
+
+/*
+ * *******************************
+ * ***** Glue tasks together *****
+ * *******************************
  */
 gulp.task('dev',
 	gulp.series(
 		'cleanServer',
-		gulp.parallel('scripts', 'styles', 'copyLibsToServer', 'html'),
+		gulp.parallel('scripts', 'css', 'copyLibsToServer', 'html'),
+		'htmllint',
 		'copyAssetsToServer'
 	)
 );
